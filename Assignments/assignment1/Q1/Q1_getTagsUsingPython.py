b607a7e6c08be8e50bc5e5db0978d89cacf6f3c1
dhirajkumar055 <dhirajkumar055@gmail.com>
2021-05-26 17:36:52 +0530

    Updated for only EC2 instances

--- Assignments/assignment1/Q1/Q1_getTagsUsingPython.py
+++ Assignments/assignment1/Q1/Q1_getTagsUsingPython.py
@@ -5,18 +5,24 @@ import boto3
 ec2=boto3.client('ec2','ap-south-1',aws_access_key_id='',aws_secret_access_key='')
 
 dictionary={}
+#print(dir(ec2.describe_tags))
 for i in ec2.describe_tags()['Tags']:
    keyValue=[]
    keyValue.append(i['Key'])
    keyValue.append(i['Value']) 
    resourceId=dictionary.get(i['ResourceId'],None)
-   if resourceId == None: 
+   if resourceId == None and i['ResourceId'].startswith("i-"): 
       dictionary[i['ResourceId']]=[]
       dictionary[i['ResourceId']].append(keyValue)
-   else:
+   elif i['ResourceId'].startswith("i-"):
       dictionary[i['ResourceId']].append(keyValue)
 
 for i in dictionary:
-   print("Tags for Resource Id "+i+" is ")
-   print(dictionary[i])
+   print("#####################################################")
+   print("Resource Id : "+i)
+   print(" ","Key".ljust(25),"Value".ljust(25)) 
+   for k,j in enumerate(dictionary[i]):
+     print(k,j[0].ljust(25),j[1].ljust(25))
+   print("#####################################################")
    print()
+
